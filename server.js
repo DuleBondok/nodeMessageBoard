d7f7ddba4995da6ef423596e414b0f1368199ee5
DuleBondok <dulebondok@gmail.com>
2025-01-29 17:44:22 +0100

    finished editing

--- server.js
+++ server.js
@@ -1,37 +1,35 @@
 const express = require('express');
 const app = express();
+const fs = require("fs");
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 app.use(express.urlencoded({extended: true}));  
 
-const messages = [
-    {
-        text: "Hi there!",
-        user: "Amando",
-        added: new Date(),
-    },
-    {
-        text: "Hello World!",
-        user: "Charles",
-        added: new Date(),
-    }
-];
+const messagesFilePath = "./messages.json";
+
+if(!fs.existsSync(messagesFilePath)) {
+    fs.writeFileSync(messagesFilePath, JSON.stringify([]));
+}
+
+let messages = JSON.parse(fs.readFileSync(messagesFilePath, "utf-8"));
 
 app.get("/", (req,res) => {
-    res.render("index", {title: "Mini Messageboard", messages: messages});
+    res.render("index", {title: "Mini Messageboard", messages});
 })
 
 app.get("/new", (req,res) => {
-    res.render("form")
+    res.render("form");
 })
 
 app.post("/new", (req,res) => {
     const {authorName, messageText} = req.body;
 
     if(authorName && messageText) {
-        messages.unshift({text: messageText, user: authorName, added: new Date()});
+        const newMessage = {text: messageText, user: authorName, added: new Date()};
+        messages.unshift(newMessage);
+        fs.writeFileSync(messagesFilePath, JSON.stringify(messages,null, 2), "utf-8");
     }
 
     res.redirect("/");
